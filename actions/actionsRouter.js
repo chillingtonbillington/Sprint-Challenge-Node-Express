9c6de024bd4ee320d4ce7d8ddd5958636d947eaa
Robert Billington <chillingtonbillington@gmail.com>
2018-12-08 12:01:39 -0600

    Completed Get by ID for actions

--- actions/actionsRouter.js
+++ actions/actionsRouter.js
@@ -3,11 +3,21 @@ const actionsDb = require('../data/helpers/actionModel');
 const router = express.Router();
 
 router.get('/', (req , res) =>{
-    actionsDb.get().then(action =>{
-        res.json(action)
+    actionsDb.get().then(actions =>{
+        res.json(actions)
     })
     .catch(err =>{
         res.status(500).json({error : 'Could not get actions'})
+    })
+})
+
+router.get('/:id', (req, res) =>{
+    const {id} = req.params;
+    actionsDb.get(id).then(action =>{
+        res.json(action)
+    })
+    .catch(err =>{
+        res.status(500).json({error: 'Could not find action by specified ID'})
     })
 })
 
